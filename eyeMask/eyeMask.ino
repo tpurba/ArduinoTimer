1df6e99a514c2ec88b72067d3907127a90d1968c
TEQ-p <takun.purba@gmail.com>
2024-01-08 13:34:04 -0700

    Finished beta version of eye mask

--- eyeMask/eyeMask.ino
+++ eyeMask/eyeMask.ino
@@ -1,11 +1,20 @@
-#include <BTAddress.h>
-#include <BTAdvertisedDevice.h>
-#include <BTScan.h>
 #include <BluetoothSerial.h>
+//library for spi and gif for lcd round display 
+#include <SPI.h>
+#include <TFT_eSPI.h>     // Install this library with the Arduino IDE Library Manager
+#include <AnimatedGIF.h>  // Install this library with the Arduino IDE Library Manager
+AnimatedGIF gif;
+//include images gif 
+#include "images/sunriseFinal.h"
+
+TFT_eSPI tft = TFT_eSPI();
 BluetoothSerial SerialBT;
-long interval = 5000;       // interval at which to print Hello (milliseconds)
+long interval;
 unsigned long currentMillis = 0;
 unsigned long wakeUpTime = 0;
+//define image 
+#define GIF_IMAGE sunriseFinal
+
 //enumerate
 enum AlarmState{
   IDLE, 
@@ -18,9 +27,13 @@ AlarmState as;
 int inputValue;
 void setup() {
   // Initialize serial communication
-  Serial.begin(9600);
+  Serial.begin(115200);
+  tft.begin();
+  tft.setRotation(2);     // Adjust Rotation of your screen (0-3)
+  tft.fillScreen(TFT_BLACK);
   as = IDLE;
   SerialBT.begin("SunRise"); // Bluetooth device name
+  gif.begin(BIG_ENDIAN_PIXELS);
 }
 
 void loop() {
@@ -40,14 +53,14 @@ void loop() {
       //Serial.println("in active state");
       currentMillis = millis();
       if(currentMillis >= wakeUpTime){
-        Serial.println("currentMillis:" + String(currentMillis));
-        Serial.println("wakeUpTime:" + String(wakeUpTime));
         as = ALARM;
       }
     break;
     case ALARM:
       //Serial.println("in alarm state");
       Serial.println("Wake Up!");
+      playImage();
+      tft.fillScreen(TFT_BLACK);
       as = IDLE;
     break;
     case ABORT:
@@ -65,4 +78,18 @@ void setAlarm(){
   currentMillis = millis();
   //set the wake up time
   wakeUpTime = currentMillis + interval;
+}
+void playImage(){
+  if (gif.open((uint8_t *)GIF_IMAGE, sizeof(GIF_IMAGE), GIFDraw))
+  {
+    Serial.printf("Successfully opened GIF; Canvas size = %d x %d\n", gif.getCanvasWidth(), gif.getCanvasHeight());
+    tft.startWrite();
+    while (gif.playFrame(true, NULL))
+    {
+      yield();
+    }
+    gif.close();
+    tft.endWrite(); 
+    Serial.println("Successfully finished gif");
+  }
 }
